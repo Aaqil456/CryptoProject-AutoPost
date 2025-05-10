3675535a2495ad095f9d37ce440560b4abdce2b4
Aaqil Ahamad <72489957+Aaqil456@users.noreply.github.com>
2025-05-10 22:31:33 +0800

    Update dashboard-init.js

--- dashboard-init.js
+++ dashboard-init.js
@@ -1,1 +1,40 @@
+jQuery(document).ready(function ($) {
+  $('#dashboard').DataTable({
+    ajax: {
+      url: 'https://aaqil456.github.io/CryptoProject-AutoPost/dashboard.json',
+      dataSrc: ''
+    },
+    columns: [
+      { data: 'nama' },
+      { data: 'dana' },
+      { data: 'fasa' },
+      { data: 'ada_token' },
+      { data: 'pelabur' },
+      { data: 'deskripsi' },
+      {
+        data: 'twitter',
+        render: function(data) {
+          if (!data || data === "-" || data.trim() === "") return "-";
+          return '<a href="https://x.com/' + data.replace('@', '') + '" target="_blank">' + data + '</a>';
+        }
+      },
+      {
+        data: 'tweet_url',
+        render: function(data) {
+          return '<a href="' + data + '" target="_blank">🔗</a>';
+        }
+      }
+    ],
+    pageLength: 5,
+    language: {
+      search: "Cari Projek:",
+      lengthMenu: "Papar _MENU_ entri",
+      info: "Menunjukkan _START_ hingga _END_ dari _TOTAL_ entri",
+      paginate: {
+        previous: "Sebelum",
+        next: "Seterusnya"
+      }
+    }
+  });
+});
 
